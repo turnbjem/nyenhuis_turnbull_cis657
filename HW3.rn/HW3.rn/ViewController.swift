802266031b0bf3bf57b40a5946929f65cc12a2e4
nyenhuir <37335851+nyenhuir@users.noreply.github.com>
2019-05-14 20:43:53 -0400

    Add files via upload

---
+++ HW3.rn/HW3.rn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW3.rn
+//
+//  Created by Reuben B. Nyenhuis on 5/14/19.
+//  Copyright © 2019 Reuben B. Nyenhuis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW3.rn/HW3.rnTests/HW3_rnTests.swift
@@ -0,0 +1,34 @@
+//
+//  HW3_rnTests.swift
+//  HW3.rnTests
+//
+//  Created by Reuben B. Nyenhuis on 5/14/19.
+//  Copyright © 2019 Reuben B. Nyenhuis. All rights reserved.
+//
+
+import XCTest
+@testable import HW3_rn
+
+class HW3_rnTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
