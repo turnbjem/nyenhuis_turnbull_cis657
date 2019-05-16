8ec7e4584be10785b85419f01e30806deff57ccf
Reuben Nyenhuis <nyenhuir@mail.gvsu.edu>
2019-05-16 13:45:29 -0400

    Added basic button press options

--- HW3 REAL/HW3 REAL/ViewController.swift
+++ HW3 REAL/HW3 REAL/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // dismiss keyboard when tapping outside oftext fields
+        let detectTouch = UITapGestureRecognizer(target: self, action:
+            #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
+        
+        // make this controller the delegate of the text fields.
+        }
+
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
     }
-
-
+    
+    @IBOutlet weak var p1lat: DecimalMinusTextField!
+    @IBOutlet weak var p1long: DecimalMinusTextField!
+    @IBOutlet weak var p2lat: DecimalMinusTextField!
+    @IBOutlet weak var p2long: DecimalMinusTextField!
+    @IBOutlet weak var distancelabel: UILabel!
+    @IBOutlet weak var bearinglabel: UILabel!
+    
+  
+    @IBAction func calculate_pressed(_ sender: Any) {
+        self.distancelabel.text = "Distance: CALCULATED"
+    }
+    
+    @IBAction func clear_pressed(_ sender: Any) {
+        self.bearinglabel.text = "Distance: CALCULATED"
+    }
 }
 
