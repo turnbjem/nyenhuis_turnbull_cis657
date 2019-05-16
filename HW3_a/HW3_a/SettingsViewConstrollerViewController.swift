8f003b460754a3668f7ceeede029112170895072
Jemima Turnbull <jemimaturnbull@Jemimas-MacBook-Air.local>
2019-05-16 18:47:35 -0400

    changes

---
+++ HW3_a/HW3_a/SettingsViewConstrollerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewConstrollerViewController.swift
+//  HW3_a
+//
+//  Created by Jemima Turnbull on 5/16/19.
+//  Copyright © 2019 Jemima Turnbull. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewConstrollerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var SettingTextField: UITextField!
+    
+    @IBAction func SaveSelected(_ sender: Any) {
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
